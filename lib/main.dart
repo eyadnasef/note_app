6e4c22d881c7f999b126a2a31e6a32d2dcb38a68
eyadnasef <workas2672007@gmail.com>
2023-11-08 09:21:44 +0200

    change theme and create notes view

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,10 @@ import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/simple_bloc_observer.dart';
-
 import 'package:notes_app/views/notes_view.dart';
-
 import 'cubits/notes_cubit/notes_cubit.dart';
 
-void main() async {
-  await Hive.initFlutter();
-
-  Bloc.observer = SimpleBlocObserver();
-  Hive.registerAdapter(NoteModelAdapter());
-  await Hive.openBox<NoteModel>(kNotesBox);
-
+void main() {
   runApp(const NotesApp());
 }
 
@@ -25,13 +17,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-       create: (context) => NotesCubit(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-        home: const NotesView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: const NotesView(),
     );
   }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -11,23 +11,6 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              context: context,
-              builder: (context) {
-                return const AddNoteBottomSheet();
-              });
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: const NotesViewBody(),
-    );
+    return Scaffold();
   }
 }
